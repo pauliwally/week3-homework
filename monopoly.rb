83727a9c672178c5f7b1b7f7b3a5f137882073ef
pauliwally <pauliwally@gmail.com>
2014-01-29 15:02:08 -0600

    HW #3

--- monopoly.rb
+++ monopoly.rb
@@ -17,22 +17,34 @@ puts cheap_property[:price]
 
 #Q2. How much does it cost to buy Boardwalk?
 #A2: Your code goes here:
-
+puts "Buying the property"
+puts rich_property[:name]
+puts "will cost you"
+puts rich_property[:price]
 
 
 #Q3. How much is rent for Baltic?
 #A3: Your code goes here:
+puts "monthly rent on"
+puts cheap_property[:name]
+puts "will cost you"
+puts cheap_property[:rent]
 
 
 
 #Q4. How much does it cost to buy New York Ave?
 #A4: Your code goes here:
+puts "Buying"
+puts more_properties.at(2)[:name]
+puts "will cost"
+puts more_properties.at(2)[:price]
 
 
 
 #Q5: How many "good" special squares are there?
 #A5: Your code goes here:
-
-
-
+puts "There are"
+puts special_squares[:good].count
+puts "good special squares in Monopoly, called"
+puts special_squares[:good]
 
